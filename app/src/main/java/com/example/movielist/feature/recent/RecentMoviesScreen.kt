7b7c2f281f4f3505f2fc1587d41dfc9f772d4231
Maciek <mck2301@gmail.com>
2024-01-12 13:17:14 +0100

    init commit

---
+++ app/src/main/java/com/example/movielist/feature/recent/RecentMoviesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.movielist.feature.recent
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecentMoviesScreen () {
+    Text(text = "")
+}
